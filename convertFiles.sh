0f8aa28ff5e929ea86ca72a0393d8b283fdad00e
Rini Banerjee <26858592+rbanerjee20@users.noreply.github.com>
2020-07-15 14:55:49 +0100

    Converted DocOnce files from all chapters to Jupyter notebooks.

--- convertFiles.sh
+++ convertFiles.sh
@@ -1,32 +1,38 @@
 #!/bin/bash
-# Script that takes DocOnce files from Waves chapter of FD method and converts into .ipynb format
+# Script that takes DocOnce files from all chapters (except waves) of FD book and converts into .ipynb format
 
-mkdir -p jupyter-fdm-book
-cd doc/.src/chapters/wave
+cd doc/.src/chapters
 
-for i in *.do.txt; do
-  doconce format ipynb $i --no_abort
-done
+CHAPTERS=("advec" "diffu" "formulas" "nonlin" "softeng2" "trunc" "vib")
 
-# Move IPython notebooks, dlog files and tar.gz files to jupyter-fdm-book directory
-mv *.ipynb ../../../../jupyter-fdm-book
-mv *.dlog ../../../../jupyter-fdm-book
-mv *.tar.gz ../../../../jupyter-fdm-book
-
-# Create an array to store temporary files
 TMPS=()
-while IFS= read -r line; do
-    TMPS+=( "$line" )
-done < <( find . -type f -name "*tmp_mako*" -name "*.do.txt" )
 
-while IFS= read -r line; do
-    TMPS+=( "$line" )
-done < <( find . -type f -name "*tmp_preprocess*" -name "*.do.txt" )
+for i in "${CHAPTERS[@]}"; do
+  cd $i
+  for j in *.do.txt; do
+    doconce format ipynb $j --no_abort
+  done
+  # Move IPython notebooks, dlog files and tar.gz files to fdm-devito-notebooks directory
+  mkdir -p ../../../../fdm-devito-notebooks/$i
+  mv *.ipynb ../../../../fdm-devito-notebooks/$i
+  rm -rf *.dlog
+  rm -rf *.tar.gz
 
-# Delete temporary files
-for i in "${TMPS[@]}"; do
-  rm -rf $i
+  # Add temporary files to TMPS
+  while IFS= read -r line; do
+      TMPS+=( "$line" )
+  done < <( find . -type f -name "*tmp_mako*" -name "*.do.txt" )
+
+  while IFS= read -r line; do
+      TMPS+=( "$line" )
+  done < <( find . -type f -name "*tmp_preprocess*" -name "*.do.txt" )
+
+  # Delete temporary files
+  for j in "${TMPS[@]}"; do
+    rm -rf $j
+  done
+  TMPS=()
+
+  cd ..
 done
 
-# cd jupyter-fdm-book
-# jupyter notebook wave1D_fd1.ipynb
