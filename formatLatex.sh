be7db4e5d2069ae462a1a56f77b38cbe2ae6667f
Rini Banerjee <26858592+rbanerjee20@users.noreply.github.com>
2020-07-21 12:54:18 +0100

    Formatted thinspace and half symbols in notebooks.

--- formatLatex.sh
+++ formatLatex.sh
@@ -1,5 +1,23 @@
+cd fdm-devito-notebooks
+
 tp="\\tp"
 newTp="\\thinspace ."
-sed -i -e "s/$tp/$newTp/g" fdm-devito-notebooks/wave/wave1D_fd2.ipynb
+half="\\half"
+newHalf="\\frac{1}{2}"
+# sed -i -e "s/$tp/$newTp/g" fdm-devito-notebooks/wave/wave1D_fd2.ipynb
 # Fix "outputs" string which is also changed by previous command
-sed -i -e "s/outhinspace .uts/outputs/g" fdm-devito-notebooks/wave/wave1D_fd2.ipynb
+# sed -i -e "s/outhinspace .uts/outputs/g" fdm-devito-notebooks/wave/wave1D_fd2.ipynb
+
+CHAPTERS=("advec" "diffu" "formulas" "nonlin" "softeng2" "trunc" "vib" "wave")
+# CHAPTERS=("wave")
+
+for i in "${CHAPTERS[@]}"; do
+  cd $i
+  for j in *.ipynb; do
+    sed -i -e "s/$tp/$newTp/g" $j
+    sed -i -e "s/outhinspace .uts/outputs/g" $j
+    sed -i -e "s/$half/$newHalf/g" $j
+  done
+  rm -rf *.ipynb-e
+  cd ..
+done
