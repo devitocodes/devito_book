17b394daa54e75b3ab7b32c3a7e0cb0db4c8b89e
Rini Banerjee <26858592+rbanerjee20@users.noreply.github.com>
2020-07-23 16:43:11 +0100

    Fixed error in formatLatex.sh and added auxilliary error arrays.

--- formatLatex.sh
+++ formatLatex.sh
@@ -9,6 +9,10 @@ DOCONCE=("\\\tp" "\\\half" "\\\Real" "\\\normalvec")
 # PURE_LATEX[i] is the pure LaTeX version of DOCONCE[i]
 PURE_LATEX=("\\\thinspace ." "\\\frac{1}{2}" "\\\mathbb{R}" "\\\boldsymbol{n}")
 
+# Auxilliary errors as a result of the above formatting and their fixes
+AUX_ERRORS=("outhinspace .ut" "athinspace .lot")
+AUX_FIXES=("output" "atplot")
+
 # Chapter directory names
 CHAPTERS=("advec" "diffu" "formulas" "nonlin" "softeng2" "trunc" "vib" "wave")
 
@@ -23,7 +27,10 @@ for chapter in "${CHAPTERS[@]}"; do
   # Only replace text in Jupyter notebooks (.ipynb files)
   for notebook in *.ipynb; do
     for index in ${!DOCONCE[@]}; do
-      sed -i -e "s/${DOCONCE[index]}/${PURE_LATEX[index]}/g" notebook
+      sed -i -e "s/${DOCONCE[index]}/${PURE_LATEX[index]}/g" $notebook
+    done
+    for index in ${!AUX_ERRORS[@]}; do
+      sed -i -e "s/${AUX_ERRORS[index]}/${AUX_FIXES[index]}/g" $notebook
     done
   done
   # Remove extra files generated 
