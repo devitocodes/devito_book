ddbb42c14cadf8c1cfc40a47aff2c5a51a699f72
Rini Banerjee <26858592+rbanerjee20@users.noreply.github.com>
2020-08-05 11:14:38 +0100

    Further experimenting with Docker.

--- docker/run-jupyter.sh
+++ docker/run-jupyter.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-PYTHONPATH=/app /venv/bin/jupyter notebook --ip=0.0.0.0 --port=8888 --allow-root
+PYTHONPATH=/app /venv/bin/xvfb-run jupyter-notebook --ip=0.0.0.0 --port=8888 --allow-root
