c83aff1ca148773b7cf6c3b982ee0cdc30af1e74
jakubbober <jakubbober.work@gmail.com>
2020-08-26 13:27:29 +0200

    Try implement first devito code block

--- fdm-devito-notebooks/04_advec/src-advec/advec1D.py
+++ fdm-devito-notebooks/04_advec/src-advec/advec1D.py
@@ -1,40 +1,69 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from devito import *
+
+# def solver_FECS(I, U0, v, L, dt, C, T, user_action=None):
+#     Nt = int(round(T/float(dt)))
+#     t = np.linspace(0, Nt*dt, Nt+1)   # Mesh points in time
+#     dx = v*dt/C
+#     Nx = int(round(L/dx))
+#     x = np.linspace(0, L, Nx+1)       # Mesh points in space
+#     # Make sure dx and dt are compatible with x and t
+#     dx = x[1] - x[0]
+#     dt = t[1] - t[0]
+#     C = v*dt/dx
+
+#     u   = np.zeros(Nx+1)
+#     u_n = np.zeros(Nx+1)
+
+#     # Set initial condition u(x,0) = I(x)
+#     for i in range(0, Nx+1):
+#         u_n[i] = I(x[i])
+
+#     if user_action is not None:
+#         user_action(u_n, x, t, 0)
+
+#     for n in range(0, Nt):
+#         # Compute u at inner mesh points
+#         for i in range(1, Nx):
+#             u[i] = u_n[i] - 0.5*C*(u_n[i+1] - u_n[i-1])
+
+#         # Insert boundary condition
+#         u[0] = U0
+
+#         if user_action is not None:
+#             user_action(u, x, t, n+1)
+
+#         # Switch variables before next step
+#         u_n, u = u, u_n
+
 
 def solver_FECS(I, U0, v, L, dt, C, T, user_action=None):
     Nt = int(round(T/float(dt)))
-    t = np.linspace(0, Nt*dt, Nt+1)   # Mesh points in time
     dx = v*dt/C
     Nx = int(round(L/dx))
-    x = np.linspace(0, L, Nx+1)       # Mesh points in space
-    # Make sure dx and dt are compatible with x and t
+    
+    t = np.linspace(0, Nt*dt, Nt+1)
+    x = np.linspace(0, L, Nx+1)
+    
     dx = x[1] - x[0]
     dt = t[1] - t[0]
     C = v*dt/dx
 
-    u   = np.zeros(Nx+1)
-    u_n = np.zeros(Nx+1)
-
-    # Set initial condition u(x,0) = I(x)
+    
+    grid = Grid(shape=(Nx + 1), extent=(L))  # TODO: should the +1 be here?
+    u = TimeFunction(name='u', grid=grid)
+    
     for i in range(0, Nx+1):
-        u_n[i] = I(x[i])
-
-    if user_action is not None:
-        user_action(u_n, x, t, 0)
-
-    for n in range(0, Nt):
-        # Compute u at inner mesh points
-        for i in range(1, Nx):
-            u[i] = u_n[i] - 0.5*C*(u_n[i+1] - u_n[i-1])
-
-        # Insert boundary condition
-        u[0] = U0
-
-        if user_action is not None:
-            user_action(u, x, t, n+1)
-
-        # Switch variables before next step
-        u_n, u = u, u_n
+        u.data[:, i] = I(x[i])
+    
+    eq = Eq(u.dt + v*u.dx)
+    
+    stencil = solve(eq, u.forward)
+    
+    op = Operator(Eq(u.forward, stencil), subdomain=grid.interior)
+    op.apply(time_M=Nt)
+    
 
 
 def solver(I, U0, v, L, dt, C, T, user_action=None,
